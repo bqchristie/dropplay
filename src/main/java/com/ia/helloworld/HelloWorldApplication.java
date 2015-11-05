8391f5dced82c460931daf718d8b560bbae5e50b
Bruce Christie <bruce.christie@mobilelive.ca>
2015-11-05 11:01:30 -0500

    First Commit

---
+++ src/main/java/com/ia/helloworld/HelloWorldApplication.java
@@ -0,0 +1,39 @@
+package com.ia.helloworld;
+
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import com.ia.helloworld.resources.HelloWorldResource;
+import com.ia.helloworld.health.TemplateHealthCheck;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/public", "/*","index.html"));
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().setUrlPattern("/api/*");
+        environment.jersey().register(resource);
+    }
+
+}
---
+++ src/main/java/com/ia/helloworld/dao/VisitsDAO.java
@@ -0,0 +1,10 @@
+package com.ia.helloworld.dao;
+
+import org.skife.jdbi.v2.sqlobject.Bind;
+
+/**
+ * Created by brucechristie on 15-11-05.
+ */
+public interface VisitsDAO {
+    void insert(@Bind("id") int id, @Bind("name") String name);
+}
